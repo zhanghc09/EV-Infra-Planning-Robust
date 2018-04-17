a07695dcaaaa7f66bcaf0faad271b282fd984e75
Zhang Hongcai <zhanghc09@126.com>
2018-04-16 22:53:29 -0700

    代码提交信息

---
+++ test.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+# the above code is used for Chinese Characters
+
+
+print "hello world!";
+if True:
+    print "True"
+    print "this is a test"
+else:
+    print "False"
+
+a=1;
+
+if a==1:
+    print("true")
+elif a==2:
+    print("does not know what to output")
+else:
+    print("wrong")
+
+print("你好！")
